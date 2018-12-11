149e0c2b9a5382823d316a751b86c303aca9cb4c
changhoonhahn <changhoonhahn@lbl.gov>
2018-12-11 11:12:25 -0800

    implementing assign2 using python wrapper; need to index 2Ngrid to Ngrid complex

--- pyspectrum/fft.py
+++ pyspectrum/fft.py
@@ -5,30 +5,38 @@ import numpy as np
 import estimator as fEstimate
 
 
-def FFTperiodic(gals, Lbox=2600., Ngrid=360): 
+def FFTperiodic(gals, Lbox=2600., Ngrid=360, silent=True): 
     '''
     '''
     kf_ks = np.float32(float(Ngrid) / Lbox)
-    #Ngrid = np.int32(Ngrid)
     Ng = np.int32(gals.shape[1]) # number of galaxies
 
-    # position of galaxies 
+    # position of galaxies (checked with fortran) 
     xyz_gals = np.zeros([3, Ng], dtype=np.float32, order='F') 
-    xyz_gals[0,:] = gals[0,:] 
-    xyz_gals[1,:] = gals[1,:]
-    xyz_gals[2,:] = gals[2,:] 
+    xyz_gals[0,:] = np.clip(gals[0,:], 0., Lbox*(1.-1e-6))
+    xyz_gals[1,:] = np.clip(gals[1,:], 0., Lbox*(1.-1e-6))
+    xyz_gals[2,:] = np.clip(gals[2,:], 0., Lbox*(1.-1e-6))
+    if not silent: print('%i galaxy positions saved' % Ng) 
 
     #delta = pyfftw.n_byte_align_empty((2*Ngrid, Ngrid, Ngrid), 16, dtype='complex64', order='F')
-    delta = np.zeros([Ngrid, Ngrid, Ngrid], dtype=np.float32, order='F') 
-    fEstimate.assign2(xyz_gals, delta, kf_ks, Ng, Ngrid) 
-    
+    _delta = np.zeros([2*Ngrid, Ngrid, Ngrid], dtype=np.float32, order='F') 
+    fEstimate.assign2(xyz_gals, _delta, kf_ks, Ng, Ngrid) 
+    if not silent: print('galaxy positions assigned to grid') 
+    delta = np.zeros([Ngrid, Ngrid, Ngrid], dtype=np.complex64, order='F') 
+    delta = _delta[::2,:,:]  # even indices are reals
+    delta = j*_delta[np.arange(1, Ngrid+1)[::2],:,:] # odds are complex 
+    return delta 
+    ''' 
     # FFT delta 
     fft_delta = np.zeros((Ngrid, Ngrid, Ngrid), dtype='complex64', order='F')
     fftw_ob = pyfftw.builders.ifftn(delta, planner_effort='FFTW_ESTIMATE')
     pyfftw.interfaces.cache.enable()
     _fft_delta = fftw_ob(delta)
     fft_delta[:,:,:] = _fft_delta.copy()
+    if not silent: print('galaxy grid FFTed') 
 
     # fcombine 
     fEstimate.fcomb(fft_delta,Ng,Ngrid) 
-    return fft_delta
+    if not silent: print('fcomb complete') 
+    return fft_delta[:Ngrid/2+1,:,:]
+    '''
--- run/test_fft.py
+++ run/test_fft.py
@@ -1,16 +1,37 @@
-'''
+''' test pyspectrum.fft and make sure it's the same as the fortran 
+code output 
 '''
 import numpy as np 
+from scipy.io import FortranFile 
 from pyspectrum import fft as FFT
 from pyspectrum import util as UT 
 
 
 if __name__=="__main__": 
-    x, y, z = np.loadtxt(''.join([UT.dat_dir(), 'BoxN1.mock100']), unpack=True, usecols=[0,1,2]) 
+
+    x, y, z = np.loadtxt(''.join([UT.dat_dir(), 'BoxN1.mock1000000']), unpack=True, usecols=[0,1,2]) 
     xyz = np.zeros((3, len(x))) 
     xyz[0,:] = x
     xyz[1,:] = y 
     xyz[2,:] = z
 
-    delta= FFT.FFTperiodic(xyz, Lbox=2600, Ngrid=10) 
+    delta = FFT.FFTperiodic(xyz, Lbox=2600, Ngrid=360, silent=False) 
+    
+    f = FortranFile(''.join([UT.dat_dir(), 'zmap.dcl']), 'r')
+    #delta_assign = f.read_reals(np.float32) 
+    #delta_assign = np.reshape(delta_assign, (3,10000000), order='F') 
+    #delta_assign = delta_assign[:,:1000000]
+    delta_assign = f.read_reals(np.complex64) 
+    delta_assign = np.reshape(delta_assign, (360, 360, 360), order='F') 
+    print np.array_equal(delta, delta_assign)
+
+    '''
+    # read in fortran code FFT output 
+    f = FortranFile(''.join([UT.dat_dir(), 'FFT.BoxN1.mock1000000.Ngrid360']), 'r')
+    _ = f.read_ints(dtype=np.int32)
+    delt = f.read_reals(dtype=np.complex64) 
+    delt = np.reshape(delt, (181, 360, 360), order='F')
+
     print delta[0,:,:]
+    print delt[0,:,:]
+    '''
