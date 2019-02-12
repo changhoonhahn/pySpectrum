5f9ec4e204c35978e2d93e6e0c5fafd110a3b0c5
changhoonhahn <changhoonhahn@lbl.gov>
2019-02-11 16:24:21 -0800

    updates to the FastPM v illustris tng bispectrum comparison

--- run/fastpm/fastpm.py
+++ run/fastpm/fastpm.py
@@ -30,36 +30,60 @@ mpl.rcParams['ytick.major.width'] = 1.5
 mpl.rcParams['legend.frameon'] = False
 
 
-def fastPM(z, str_flag='', Lbox=205., Nmax=40, Ncut=3, step=3):     
+def fastPM(z, str_flag='', mh_lim=15., Lbox=205., Nmax=40, Ncut=3, step=3):     
     ''' calculate the powerspectrum and bispectrum of the fastPM catalog.
     '''
     dir_fpm = os.path.join(UT.dat_dir(), 'fastpm') 
     f_halo = ('halocat_FastPM_40step_N250_IC500_B2_z%.2f%s.txt' % (z, str_flag))
-    f_pell = ('%s/pySpec.Plk.%s.Lbox%.f.Ngrid360.dat' % (dir_fpm, f_halo, Lbox))
-    f_pnkt = ('%s/pySpec.Plk.%s.Lbox%.f.Ngrid360.nbodykit.dat' % (dir_fpm, f_halo, Lbox))
-    f_b123 = ('%s/pySpec.Bk.%s.Lbox%.f.Ngrid360.step%i.Ncut%i.Nmax%i.dat' % (dir_fpm, f_halo, Lbox, step, Ncut, Nmax))
+    f_mlim = ('halocat_FastPM_40step_N250_IC500_B2_z%.2f%s.mlim%.fe10' % (z, str_flag, mh_lim))
+    f_hdf5 = ('%s/%s.hdf5' % (dir_fpm, f_mlim)) 
+    f_pell = ('%s/pySpec.Plk.%s.Lbox%.f.Ngrid360.dat' % (dir_fpm, f_mlim, Lbox))
+    f_pnkt = ('%s/pySpec.Plk.%s.Lbox%.f.Ngrid360.nbodykit.dat' % (dir_fpm, f_mlim, Lbox))
+    f_b123 = ('%s/pySpec.Bk.%s.Lbox%.f.Ngrid360.step%i.Ncut%i.Nmax%i.dat' % (dir_fpm, f_mlim, Lbox, step, Ncut, Nmax))
 
     kf = 2.*np.pi/Lbox
     
-    # read in halo catalog
-    dat_halo = np.loadtxt(os.path.join(dir_fpm, f_halo), unpack=True, usecols=[0,1,2,3,7,8,9]) 
-    mh = dat_halo[0]
-    Nhalo = len(mh) 
-    print('%i halos in %.f Mpc/h box' % (Nhalo, Lbox))
-    print('%f < M_h/10^10Msun < %f' % (mh.min(), mh.max()))
-    xyz = np.zeros((Nhalo,3)) 
-    xyz[:,0] = dat_halo[1]
-    xyz[:,1] = dat_halo[2]
-    xyz[:,2] = dat_halo[3]
-    print('%f < x < %f' % (xyz[:,0].min(), xyz[:,0].max()))
-    print('%f < y < %f' % (xyz[:,1].min(), xyz[:,1].max()))
-    print('%f < z < %f' % (xyz[:,2].min(), xyz[:,2].max()))
+    if not os.path.isfile(f_hdf5): 
+        # read in halo catalog
+        dat_halo = np.loadtxt(os.path.join(dir_fpm, f_halo), unpack=True, usecols=[0,1,2,3,7,8,9]) 
+        mh = dat_halo[0]
+        Nhalo = len(mh) 
+        print('%i halos in %.f Mpc/h box' % (len(mh), Lbox))
+        print('%f < M_h/10^10Msun < %f' % (mh.min(), mh.max()))
+        xyz = np.zeros((Nhalo,3)) 
+        xyz[:,0] = dat_halo[1]
+        xyz[:,1] = dat_halo[2]
+        xyz[:,2] = dat_halo[3]
+        print('%f < x < %f' % (xyz[:,0].min(), xyz[:,0].max()))
+        print('%f < y < %f' % (xyz[:,1].min(), xyz[:,1].max()))
+        print('%f < z < %f' % (xyz[:,2].min(), xyz[:,2].max()))
 
-    vxyz = np.zeros((Nhalo,3))
-    vxyz[:,0] = dat_halo[4]
-    vxyz[:,1] = dat_halo[5] 
-    vxyz[:,2] = dat_halo[6]
-    
+        vxyz = np.zeros((Nhalo,3))
+        vxyz[:,0] = dat_halo[4]
+        vxyz[:,1] = dat_halo[5] 
+        vxyz[:,2] = dat_halo[6]
+
+        mlim = (mh > 15.) 
+        Nhalo = np.sum(mlim) 
+        print('%i halos in %.f Mpc/h box with Mh > %f' % (Nhalo, Lbox, mh_lim))
+        
+        mh = mh[mlim] 
+        xyz = xyz[mlim,:] 
+        vxyz = vxyz[mlim,:]
+
+        f = h5py.File(f_hdf5, 'w') 
+        f.create_dataset('xyz', data=xyz) 
+        f.create_dataset('vxyz', data=vxyz) 
+        f.create_dataset('mhalo', data=mh) 
+        f.close() 
+    else: 
+        f = h5py.File(f_hdf5, 'r') 
+        xyz = f['xyz'].value
+        vxyz = f['vxyz'].value
+        mh = f['mhalo'].value
+        Nhalo = xyz.shape[0]
+        print('%i halos in %.f Mpc/h box with Mh > %f' % (len(mh), Lbox, mh_lim))
+
     nhalo = float(Nhalo) / Lbox**3
     print('number density = %f' % nhalo) 
     print('1/nbar = %f' % (1./nhalo))
@@ -153,7 +177,7 @@ def fastPM(z, str_flag='', Lbox=205., Nmax=40, Ncut=3, step=3):
     sub.plot(i_k[iksort] * kf, p0k1[iksort], c='k', lw=1, ls='--', label='bispectrum code') 
     sub.legend(loc='lower left', fontsize=20) 
     sub.set_ylabel('$P_0(k)$', fontsize=25) 
-    #sub.set_ylim([1e2, 3e4]) 
+    sub.set_ylim([1e0, 1e4]) 
     sub.set_yscale('log') 
     sub.set_xlabel('$k$', fontsize=25) 
     sub.set_xlim([1e-2, 10.]) 
@@ -206,33 +230,57 @@ def fastPM(z, str_flag='', Lbox=205., Nmax=40, Ncut=3, step=3):
     return None
 
 
-def TNG(z, Lbox=205., Nmax=40, Ncut=3, step=3):     
+def TNG(z, mh_lim=15., Lbox=205., Nmax=40, Ncut=3, step=3):     
     ''' calculate the powerspectrum and bispectrum of the fastPM catalog.
     '''
     dir_fpm = os.path.join(UT.dat_dir(), 'fastpm') 
     f_halo = ('halocat_TNG300Dark_z%.2f.txt' % z) 
-    f_pell = ('%s/pySpec.Plk.%s.Lbox%.f.Ngrid360.dat' % (dir_fpm, f_halo, Lbox))
-    f_pnkt = ('%s/pySpec.Plk.%s.Lbox%.f.Ngrid360.nbodykit.dat' % (dir_fpm, f_halo, Lbox))
-    f_b123 = ('%s/pySpec.Bk.%s.Lbox%.f.Ngrid360.step%i.Ncut%i.Nmax%i.dat' % (dir_fpm, f_halo, Lbox, step, Ncut, Nmax))
+    f_mlim = ('halocat_TNG300Dark_z%.2f.mlim%.fe10' % (z, mh_lim)) 
+    f_hdf5 = ('%s/%s.hdf5' % (dir_fpm, f_mlim)) 
+    f_pell = ('%s/pySpec.Plk.%s.Lbox%.f.Ngrid360.dat' % (dir_fpm, f_mlim, Lbox))
+    f_pnkt = ('%s/pySpec.Plk.%s.Lbox%.f.Ngrid360.nbodykit.dat' % (dir_fpm, f_mlim, Lbox))
+    f_b123 = ('%s/pySpec.Bk.%s.Lbox%.f.Ngrid360.step%i.Ncut%i.Nmax%i.dat' % (dir_fpm, f_mlim, Lbox, step, Ncut, Nmax))
 
     kf = 2.*np.pi/Lbox
     
-    # read in halo catalog
-    dat_halo = np.loadtxt(os.path.join(dir_fpm, f_halo), unpack=True, usecols=[0,1,2,3,7,8,9]) 
-    mh = dat_halo[0]
-    Nhalo = len(mh) 
-    print('%i halos in %.f Mpc/h box' % (Nhalo, Lbox))
-    print('%f < M_h/10^10Msun < %f' % (mh.min(), mh.max()))
-    xyz = np.zeros((Nhalo,3)) 
-    xyz[:,0] = dat_halo[1]
-    xyz[:,1] = dat_halo[2]
-    xyz[:,2] = dat_halo[3]
+    if not os.path.isfile(f_hdf5): 
+        # read in halo catalog
+        dat_halo = np.loadtxt(os.path.join(dir_fpm, f_halo), unpack=True, usecols=[0,1,2,3,7,8,9]) 
+        mh = dat_halo[0]
+        Nhalo = len(mh) 
+        print('%i halos in %.f Mpc/h box' % (Nhalo, Lbox))
+        print('%f < M_h/10^10Msun < %f' % (mh.min(), mh.max()))
+        xyz = np.zeros((Nhalo,3)) 
+        xyz[:,0] = dat_halo[1]
+        xyz[:,1] = dat_halo[2]
+        xyz[:,2] = dat_halo[3]
 
-    vxyz = np.zeros((Nhalo,3))
-    vxyz[:,0] = dat_halo[4]
-    vxyz[:,1] = dat_halo[5] 
-    vxyz[:,2] = dat_halo[6]
-    
+        vxyz = np.zeros((Nhalo,3))
+        vxyz[:,0] = dat_halo[4]
+        vxyz[:,1] = dat_halo[5] 
+        vxyz[:,2] = dat_halo[6]
+
+        mlim = (mh > 15.) 
+        Nhalo = np.sum(mlim) 
+        print('%i halos in %.f Mpc/h box with Mh > %f' % (Nhalo, Lbox, mh_lim))
+        
+        mh = mh[mlim] 
+        xyz = xyz[mlim,:] 
+        vxyz = vxyz[mlim,:]
+
+        f = h5py.File(f_hdf5, 'w') 
+        f.create_dataset('xyz', data=xyz) 
+        f.create_dataset('vxyz', data=vxyz) 
+        f.create_dataset('mhalo', data=mh) 
+        f.close() 
+    else: 
+        f = h5py.File(f_hdf5, 'r') 
+        xyz = f['xyz'].value
+        vxyz = f['vxyz'].value
+        mh = f['mhalo'].value
+        Nhalo = xyz.shape[0]
+        print('%i halos in %.f Mpc/h box with Mh > %f' % (len(mh), Lbox, mh_lim))
+
     nhalo = float(Nhalo) / Lbox**3
     print('number density = %f' % nhalo) 
     print('1/nbar = %f' % (1./nhalo))
@@ -380,13 +428,15 @@ def TNG(z, Lbox=205., Nmax=40, Ncut=3, step=3):
     return None
 
 
-def fastpm_v_tng(z, Lbox=205., Nmax=40, Ncut=3, step=3): 
+def fastpm_v_tng(z, str_flag='', mh_lim=15., Lbox=205., Nmax=40, Ncut=3, step=3): 
     ''' make plots that compare the powerspectrum and bispectrum of 
     fastpm and illustris tng 
     '''
     dir_fpm = os.path.join(UT.dat_dir(), 'fastpm') 
-    f_fpm = ('halocat_FastPM_40step_N250_IC500_B2_z%.2f%s.txt' % (z, str_flag))
-    f_tng = ('halocat_TNG300Dark_z%.2f.txt' % z) 
+    f_fpm = ('halocat_FastPM_40step_N250_IC500_B2_z%.2f%s.mlim%.fe10' % (z, str_flag, mh_lim))
+    f_tng = ('halocat_TNG300Dark_z%.2f.mlim%.fe10' % (z, mh_lim)) 
+    print('FastPM file: %s' % f_fpm) 
+    print('Illustris TNG file: %s' % f_tng) 
 
     f_pell = lambda f_halo: ('%s/pySpec.Plk.%s.Lbox%.f.Ngrid360.dat' % (dir_fpm, f_halo, Lbox))
     f_pnkt = lambda f_halo: ('%s/pySpec.Plk.%s.Lbox%.f.Ngrid360.nbodykit.dat' % (dir_fpm, f_halo, Lbox))
@@ -394,80 +444,75 @@ def fastpm_v_tng(z, Lbox=205., Nmax=40, Ncut=3, step=3):
 
     kf = 2.*np.pi/Lbox
     
+    # P(k) comparison 
     fig = plt.figure(figsize=(5,5))
     sub = fig.add_subplot(111)
     k, p0k = np.loadtxt(f_pell(f_fpm), unpack=True, skiprows=1, usecols=[0,1]) 
-    klim = (k < 0.3) 
-    sub.plot(k[klim], p0k[klim], c='k', lw=1, label='FastPM') 
-    k, p0k = np.loadtxt(f_pnkt(f_fpm), unpack=True, skiprows=1, usecols=[0,1]) 
-    klim = (k < 0.3) 
-    sub.plot(k[klim], p0k[klim], c='k', ls='--', lw=1) 
+    sub.plot(k, p0k, c='k', lw=1, label='FastPM') 
+    #k, p0k = np.loadtxt(f_pnkt(f_fpm), unpack=True, skiprows=1, usecols=[0,1]) 
+    #sub.plot(k, p0k, c='k', ls='--', lw=1) 
     k, p0k = np.loadtxt(f_pell(f_tng), unpack=True, skiprows=1, usecols=[0,1]) 
-    klim = (k < 0.3) 
-    sub.plot(k[klim], p0k[klim], c='C1', lw=1, label='Illustris TNG') 
-    k, p0k = np.loadtxt(f_pnkt(f_tng), unpack=True, skiprows=1, usecols=[0,1]) 
-    klim = (k < 0.3) 
-    sub.plot(k[klim], p0k[klim], c='C1', ls='--', lw=1) 
-    sub.legend(loc='upper right', fontsize=20) 
+    sub.plot(k, p0k, c='C1', lw=1, label='Illustris TNG') 
+    #k, p0k = np.loadtxt(f_pnkt(f_tng), unpack=True, skiprows=1, usecols=[0,1]) 
+    #sub.plot(k, p0k, c='C1', ls='--', lw=1) 
+    sub.legend(loc='lower left', fontsize=20) 
     sub.set_ylabel('$P_0(k)$', fontsize=25) 
     sub.set_yscale('log') 
+    sub.set_ylim([1e0, 1e4]) 
     sub.set_xlabel('$k$', fontsize=25) 
-    sub.set_xlim([8e-3, 0.5]) 
     sub.set_xscale('log') 
-    fig.savefig(os.path.join(dir_fpm, 'p0k_fpm_tng_z', bbox_inches='tight')
-    fig.savefig(''.join([UT.dat_dir(), 'qpm/p0k_qpm_aemulus', str_rsd, '.png']), bbox_inches='tight')
+    sub.set_xlim([1e-2, 10.]) 
+    fig.savefig(os.path.join(dir_fpm, 'p0k_fpm_tng_z%.2f%s.png' % (z, str_flag)), bbox_inches='tight')
 
     fig = plt.figure(figsize=(10,5))
     sub = fig.add_subplot(111)
-    i_k, j_k, l_k, p0k1, p0k2, p0k3, b123, q123, counts, b123_sn = np.loadtxt(f_b123('aemulus'),
-            skiprows=1, unpack=True, usecols=range(10)) 
-    klim = ((i_k*kf_aem <= 0.22) & (i_k*kf_aem >= 0.03) &
-            (j_k*kf_aem <= 0.22) & (j_k*kf_aem >= 0.03) & 
-            (l_k*kf_aem <= 0.22) & (l_k*kf_aem >= 0.03)) 
-    i_k, j_k, l_k = i_k[klim], j_k[klim], l_k[klim]
-        
-    ijl = UT.ijl_order(i_k, j_k, l_k, typ='GM') # order of triangles 
-   
-    sub.scatter(range(np.sum(klim)), b123[klim][ijl], c='k', s=5, label='Aemulus') 
-    sub.plot(range(np.sum(klim)), b123[klim][ijl], c='k') 
+    for fh, lbl, c in zip([f_fpm, f_tng], ['FastPM', 'Illustris TNG'], ['k', 'C1']): 
+        i_k, j_k, l_k, p0k1, p0k2, p0k3, b123, q123, counts, b123_sn = np.loadtxt(f_b123(fh), 
+                skiprows=1, unpack=True, usecols=range(10)) 
+        klim = ((i_k * kf <= 1.) & (i_k * kf >= 0.01) &
+                (j_k * kf <= 1.) & (j_k * kf >= 0.01) & 
+                (l_k * kf <= 1.) & (l_k * kf >= 0.01)) 
 
-    i_k, j_k, l_k, p0k1, p0k2, p0k3, b123, q123, counts, b123_sn = np.loadtxt(f_b123('qpm'),
-            skiprows=1, unpack=True, usecols=range(10)) 
-    klim = ((i_k*kf_qpm <= 0.22) & (i_k*kf_qpm >= 0.03) &
-            (j_k*kf_qpm <= 0.22) & (j_k*kf_qpm >= 0.03) & 
-            (l_k*kf_qpm <= 0.22) & (l_k*kf_qpm >= 0.03)) 
-    i_k, j_k, l_k, = i_k[klim], j_k[klim], l_k[klim]
-    
-    ijl = UT.ijl_order(i_k, j_k, l_k, typ='GM') # order of triangles 
-    sub.scatter(range(np.sum(klim)), b123[klim][ijl], c='C1', s=5, label='QPM') 
-    sub.plot(range(np.sum(klim)), b123[klim][ijl], c='C1') 
+        i_k, j_k, l_k = i_k[klim], j_k[klim], l_k[klim]
+        ijl = UT.ijl_order(i_k, j_k, l_k, typ='GM') # order of triangles 
+   
+        sub.scatter(range(np.sum(klim)), b123[klim][ijl], c=c, s=5, label=lbl) 
+        sub.plot(range(np.sum(klim)), b123[klim][ijl], c=c) 
+
     sub.legend(loc='upper right', markerscale=4, handletextpad=0., fontsize=20) 
     sub.set_ylabel('$B(k_1, k_2, k_3)$', fontsize=25) 
     sub.set_yscale('log') 
-    sub.set_ylim([1e8, 6e9]) 
+    sub.set_ylim([1e3, 5e6]) 
     sub.set_xlabel(r'$k_1 \le k_2 \le k_3$ triangle indices', fontsize=25) 
     sub.set_xlim([0, np.sum(klim)])
-    fig.savefig(''.join([UT.dat_dir(), 'qpm/B123_qpm_aemulus', str_rsd, '.png']), bbox_inches='tight')
+    fig.savefig(os.path.join(dir_fpm, 'bk_fpm_tng_z%.2f%s.png' % (z, str_flag)), bbox_inches='tight')
+
 
     fig = plt.figure(figsize=(10,5))
     sub = fig.add_subplot(111)
-    i_k, j_k, l_k, p0k1, p0k2, p0k3, b123, q123, counts, b123_sn = np.loadtxt(f_b123('aemulus'),
-            skiprows=1, unpack=True, usecols=range(10)) 
-    sub.scatter(range(len(q123)), q123, c='k', s=2, label='Aemulus') 
-    i_k, j_k, l_k, p0k1, p0k2, p0k3, b123, q123, counts, b123_sn = np.loadtxt(f_b123('qpm'),
-            skiprows=1, unpack=True, usecols=range(10)) 
-    sub.scatter(range(len(q123)), q123, c='C1', s=2, label='QPM') 
-    sub.legend(loc='upper right', fontsize=20) 
+    for fh, lbl, c in zip([f_fpm, f_tng], ['FastPM', 'Illustris TNG'], ['k', 'C1']): 
+        i_k, j_k, l_k, p0k1, p0k2, p0k3, b123, q123, counts, b123_sn = np.loadtxt(f_b123(fh), 
+                skiprows=1, unpack=True, usecols=range(10)) 
+        klim = ((i_k *kf >= 0.01) & (j_k *kf >= 0.01) & (l_k *kf >= 0.01)) 
+        i_k, j_k, l_k = i_k[klim], j_k[klim], l_k[klim]
+        ijl = UT.ijl_order(i_k, j_k, l_k, typ='GM') # order of triangles 
+   
+        sub.scatter(range(np.sum(klim)), q123[klim][ijl], c=c, s=5, label=lbl) 
+        sub.plot(range(np.sum(klim)), q123[klim][ijl], c=c) 
+
+    sub.legend(loc='upper right', markerscale=4, handletextpad=0., fontsize=20) 
     sub.set_ylabel('$Q(k_1, k_2, k_3)$', fontsize=25) 
-    sub.set_xlabel('$k_1 > k_2 > k_3$ triangle indices', fontsize=25) 
-    sub.set_xlim([0, len(q123)]) 
-    fig.savefig(''.join([UT.dat_dir(), 'qpm/Q123_qpm_aemulus', str_rsd, '.png']), bbox_inches='tight')
+    sub.set_ylim([0., 1.]) 
+    sub.set_xlabel(r'$k_1 \le k_2 \le k_3$ triangle indices', fontsize=25) 
+    sub.set_xlim([0, np.sum(klim)])
+    fig.savefig(os.path.join(dir_fpm, 'qk_fpm_tng_z%.2f%s.png' % (z, str_flag)), bbox_inches='tight')
     return None
 
 
 if __name__=="__main__": 
     for z in [0., 0.52, 1.04, 2.]: 
         #fastPM(z)
-        TNG(z) 
-    #QPM_AEM(rsd=False)
-    #QPM_AEM(rsd=True)
+        #fastPM(z, str_flag='_calibratebias')
+        #TNG(z) 
+        #fastpm_v_tng(z, Lbox=205., Nmax=40, Ncut=3, step=3)
+        fastpm_v_tng(z, str_flag='_calibratebias', Lbox=205., Nmax=40, Ncut=3, step=3)
