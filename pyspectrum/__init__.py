846da79468a7b755580ad8ab0656d903fb16b6b7
changhoonhahn <changhoonhahn@lbl.gov>
2020-02-03 13:02:07 -0800

    trying to resolve packaging issues

--- pyspectrum/__init__.py
+++ pyspectrum/__init__.py
@@ -1,15 +1,7 @@
 import os 
 
 # Path to data files required for cosmos
-_PYSPEC_DAT_DIR=os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'dat')
-_PYSPEC_FIG_DIR=os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'fig')
-_PYSPEC_DOC_DIR=os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'doc')
+_PYSPEC_DAT_DIR=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dat')
 
 def dat_dir():
     return _PYSPEC_DAT_DIR
-
-def fig_dir():
-    return _PYSPEC_FIG_DIR
-
-def doc_dir():
-    return _PYSPEC_DOC_DIR
