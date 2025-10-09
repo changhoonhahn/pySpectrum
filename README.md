0725d741d4785b47c8b1a48444e9885e3459f4b2
ChangHoon Hahn <chhahn@arizona.edu>
2025-10-09 10:14:31 -0500

    install explanation in readme; setup.py cleaned up

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Python package for calculating the galaxy/halo/dark matter power spectrum and bi
 - [Coming Soon](#coming-soon)
 - [Contact](#contact) 
 
-
 ## Installation
 required packages: 
 * numpy 
@@ -21,11 +20,64 @@ To install the package, clone the github repo and run setpy.py
 ```bash
 git clone https://github.com/changhoonhahn/pySpectrum.git
 cd pySpectrum
+```
+
+At the moment installation is quite janky. You'll need to modify the following
+lines in `setup.py` to point to your fftw3: 
+```python 
+ext = Extension(name='estimator', 
+        sources=['pyspectrum/estimator.f'], 
+        language='f77', 
+        library_dirs = ["/usr/local/lib"], # modify this 
+        libraries = ['fftw3f'], 
+        include_dirs=[np.get_include(), '/usr/local/include'], # modify this 
+        extra_f77_compile_args=['-fcheck=all', '-fallow-argument-mismatch'])
+```
+I've listed some examples on different machines in [setup.py examples](#setup.py-examples). 
+Once you've modified `setup.py` run 
+
+```bash 
 python setup.py install
 ```
 
 ## Coming Soon
-*  power spectrum quadrupole for periodic box
+* power spectrum quadrupole for periodic box
+* less janky install 
 
 ## Contact
-If you have any questions or need help using the package, feel free to contact me at changhoon.hahn@princeton.edu
+If you have any questions or need help using the package, feel free to contact me at changhoon.hahn@utexas.edu
+
+
+-----
+## setup.py examples 
+**mac with FFTW installed using homebrew**
+```python 
+ext = Extension(name='estimator', 
+        sources=['pyspectrum/estimator.f'], 
+        language='f77', 
+        library_dirs = ["/opt/homebrew/Cellar/fftw/3.3.10_2/lib"],
+        libraries = ['fftw3f'], 
+        include_dirs=[np.get_include(), '/opt/homebrew/Cellar/fftw/3.3.10_2/include'], 
+        extra_f77_compile_args=['-fcheck=all', '-fallow-argument-mismatch'])
+```
+
+**NERSC Cori**
+```python
+ext = Extension(name='estimator', 
+        sources=['pyspectrum/estimator.f'], 
+        language='f77', 
+        library_dirs = ["/opt/cray/pe/fftw/default/x86_64/lib"],
+        libraries = ['fftw3f'], 
+        include_dirs=[np.get_include(), "/opt/cray/pe/fftw/default/x86_64/include"])
+```
+
+**Princeton Tiger cluster**
+```python 
+ext = Extension(name='estimator', 
+        sources=['pyspectrum/estimator.f'], 
+        language='f77', 
+        library_dirs = ["/usr/local/fftw/intel-16.0/3.3.4/lib64"], 
+        libraries = ['fftw3f'], 
+        include_dirs=[np.get_include(), "/usr/local/fftw/intel-16.0/3.3.4/include"])
+```
+
--- setup.py
+++ setup.py
@@ -3,60 +3,22 @@ import numpy as np
 from numpy.distutils.core import setup
 from numpy.distutils.core import Extension
 
-__version__ = '0.0'
+__version__ = '0.1'
 
-try: 
-    if 'NERSC_HOST' in os.environ.keys(): 
-        if os.environ['NERSC_HOST'] == 'edison': 
-            ext = Extension(name='estimator', 
-                    sources=['pyspectrum/estimator.f'], 
-                    language='f77', 
-                    library_dirs = ["/opt/cray/pe/fftw/3.3.8.1/x86_64/lib"],
-                    libraries = ['fftw3f'], 
-                    include_dirs=[np.get_include(), "/opt/cray/pe/fftw/3.3.8.1/x86_64/include"])
-        elif os.environ['NERSC_HOST'] == 'cori': 
-            ext = Extension(name='estimator', 
-                    sources=['pyspectrum/estimator.f'], 
-                    language='f77', 
-                    library_dirs = ["/opt/cray/pe/fftw/default/x86_64/lib"],
-                    libraries = ['fftw3f'], 
-                    include_dirs=[np.get_include(), "/opt/cray/pe/fftw/default/x86_64/include"])
-        else: 
-            raise KeyError
-    elif 'machine' in os.environ.keys(): 
-        if os.environ['machine'] == 'tiger': 
-            print('install on princeton Tiger') 
-            ext = Extension(name='estimator', 
-                    sources=['pyspectrum/estimator.f'], 
-                    language='f77', 
-                    library_dirs = ["/usr/local/fftw/intel-16.0/3.3.4/lib64"], 
-                    libraries = ['fftw3f'], 
-                    include_dirs=[np.get_include(), "/usr/local/fftw/intel-16.0/3.3.4/include"])
-        elif os.environ['machine'] == 'mbp': 
-            ext = Extension(name='estimator', 
-                    sources=['pyspectrum/estimator.f'], 
-                    language='f77', 
-                    library_dirs = ["/usr/local/lib"],
-                    libraries = ['fftw3f'], 
-                    include_dirs=[np.get_include(), '/usr/local/include'],
-                    extra_f77_compile_args=['-fcheck=all', '-fallow-argument-mismatch'])
-    else: 
-        raise KeyError
-except KeyError: 
-    ext = Extension(name='estimator', 
-            sources=['pyspectrum/estimator.f'], 
-            language='f77', 
-            library_dirs = ["/usr/local/lib"],
-            libraries = ['fftw3f'], 
-            include_dirs=[np.get_include(), '/usr/local/include'],
-            extra_f77_compile_args=['-fcheck=all', '-fallow-argument-mismatch'])
+ext = Extension(name='estimator', 
+        sources=['pyspectrum/estimator.f'], 
+        language='f77', 
+        library_dirs = ["/usr/local/lib"],
+        libraries = ['fftw3f'], 
+        include_dirs=[np.get_include(), '/usr/local/include'],
+        extra_f77_compile_args=['-fcheck=all', '-fallow-argument-mismatch'])
 
 if __name__=="__main__": 
     setup(name = 'pySpectrum',
           version = __version__,
           description = 'TBD',
           author='ChangHoon Hahn',
-          author_email='hahn.changhoon@gmail.com',
+          author_email='changhoon.hahn@utexas.edu',
           url='',
           package_data={'pyspectrum': ['dat/fftw3.f', 'dat/*.pyfftw', 'dat/test_box.hdf5']},
           platforms=['*nix'],
